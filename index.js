370aabe2e69f3d78ea875f55c0debc4115de3fed
Rishabh-jain-21 <79372016+Rishabh-jain-21@users.noreply.github.com>
2021-05-30 23:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,16 +16,51 @@ else if(buttonText=='AC')
 {
 screen.value='';
 screenValue='';
+
 }
+
 else if(buttonText=='=')
 {
     screen.value = eval(screenValue);
+    screenValue = screen.value
 }
-else 
+else if(buttonText == 'C')
+{
+    str = screenValue;
+    str = str.toString();
+    var a = str.slice(0 , -1);
+    screenValue = a;
+    screen.value = a;
+}
+else if(buttonText =='sin')
+{
+    screen.value = Math.sin(screenValue);
+    screenValue = screenValue;
+}
+else if(buttonText =='cos')
+{
+    screen.value = Math.cos(screenValue);
+    screenValue = screen.value;
+}
+else if(buttonText =='tan')
+{
+    screen.value = Math.tan(screenValue);
+    screenValue = screen.value;
+}
+else if(buttonText =='sqr')
+{
+    screen.value = screenValue * screenValue;
+    screenValue = screen.value;
+}
+else
 {
     screenValue += buttonText;
     screen.value = screenValue;
-
 }
     })
 }
+var menu = document.getElementById(menubar);
+menu.addEventListener('click' , (t)=>
+{
+    alert('This feature will be avalible soon Thanks for Using');
+})
